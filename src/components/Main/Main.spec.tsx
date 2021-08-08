0d4a33f509fe1eff331df5f45e7f0ddf5eed7a3d
Henrique Tavares <ihenrits@gmail.com>
2021-08-07 23:34:35 -0300

    criando estilos no primeiro componente

--- src/components/Main/Main.spec.tsx
+++ src/components/Main/Main.spec.tsx
@@ -12,4 +12,12 @@ describe('<Main />', () => {
       }),
     ).toBeInTheDocument();
   });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+    });
+  });
 });
